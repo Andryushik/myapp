160e9a3a9ebd2a471b36d5e63d8e9f05ddb41869
Andrei <andryushik@gmail.com>
2023-11-21 11:42:40 +0100

    les 9 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "about", to: "about#index"
+  
+  root to: "main#index" 
+  #better to use root for mainpage but can use get "/" or get""
+  
+  get "about-us", to: "about#index", as: :about
+  # we can use standart way also: get "about", to: "about#index" but this one shows url about-us and render about
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  
   get "up" => "rails/health#show", as: :rails_health_check
-
   # Defines the root path route ("/")
   # root "posts#index"
 end
+
