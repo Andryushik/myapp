2dcd12767a9675a801b0b9921884e5c01b8b3a76
Andrei <andryushik@gmail.com>
2023-11-22 18:47:18 +0100

    les 22 done

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,22 @@ Rails.application.routes.draw do
   # we can use standart way also: get "about", to: "about#index" but this one shows url about-us and render about
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
   
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
 
   root to: "main#index" 
   #better to use root for mainpage but can use get "/" or get""
@@ -17,3 +30,4 @@ Rails.application.routes.draw do
   # root "posts#index"
 end
 
+ 
