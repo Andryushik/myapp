ca079b019e9e17a44a1204db4b4369d15549183a
Andrei <andryushik@gmail.com>
2023-11-22 11:08:01 +0100

    les 15

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,8 @@
 class MainController < ApplicationController
   def index
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end
     # alert messages example
     # flash.now[:notice] = "Logged in successfully" 
     # flash.now[:alert] = "Invalid email or password" 
